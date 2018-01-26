04b427f19baabde202c73ac62801c23c10cbf156
Tabish Raza <tabishraza@gmail.com>
2018-01-26 13:12:17 +0000

    crud for movies

--- BackEnd/controller/movieController.js
+++ BackEnd/controller/movieController.js
@@ -1,9 +1,50 @@
 import mongoose from 'mongoose';
-import { ContactSchema } from '../models/crmModel';
+import { MovieSchema } from '../models/movieModel';
 
 const Movie = mongoose.model('Movie', MovieSchema);
 
+export const addNewMovie = (req, res) => {
+  let newMovie = new Movie(req.body);
+  newMovie.save((err, movie) => {
+    if(err){
+      res.send(err);
+    }
+    res.json(movie);
+  });
+};
 
-export const listAllMovies = (req,res) => {
-    
-}
+export const getMovies = (req, res) => {
+  Movie.find({}, (err, movie) => {
+    if(err){
+      res.send(err);
+    }
+    res.json(movie);
+  });
+};
+
+export const getMovieWithId = (req, res) => {
+  Movie.findById(req.params.movieId, (err, movie) => {
+    if(err){
+      res.send(err);
+    }
+    res.json(movie);
+  });
+};
+
+export const updateMovie = (req, res) => {
+  Movie.findOneAndUpdate({_id: req.params.movieId }, req.body, { new: true }, (err, movie) => {
+    if(err){
+      res.send(err)
+    }
+    res.json(movie)
+  });
+};
+
+export const removeMovie = (req, res) => {
+  Movie.remove({_id: req.params.movieId}, (err, movie) => {
+    if(err){
+      res.send(err)
+    }
+    res.json({ message: 'Movie successfully deleted' });
+  });
+};
